f75c5d59fccc11e5d98e535b9975364a367176d6
fran2005 <franco.brossi@yahoo.com>
2026-03-24 16:29:24 -0300

    migrando mi array de objetos a un archivo json simulando una api, ademas cambie los onclick por addeventlistener

--- js/main.js
+++ js/main.js
@@ -1,19 +1,10 @@
 
 // Hola Fran, gracias por enviar tu pre entrega 2 , en general muy buen avance. La entrega cumple con los requisitos de la consigna al integrar JavaScript con HTML mediante manipulación del DOM y uso de eventos. Los productos se renderizan dinámicamente en la página, se capturan eventos en los botones para agregar o eliminar productos del carrito y se actualiza el contenido del carrito directamente en la interfaz. También se utiliza un array para almacenar los productos seleccionados y localStorage para guardar la información y recuperarla al recargar la página. El flujo de entrada, proceso y salida es claro y las funciones están separadas para tareas específicas como renderizar productos, actualizar el carrito y calcular el total. Para alcanzar un nivel óptimo, sería recomendable mejorar algunos detalles de organización del código, por ejemplo manejar todos los eventos con addEventListener en lugar de mezclarlo con onclick, y mantener un criterio más uniforme en la manipulación del DOM. También podría agregarse un manejo de cantidades en el carrito en lugar de repetir productos iguales en el array
 
-const productosDisponibles = [
-    { id: 1, nombre: "Escobas", precio: 7000, imagen:"./assets/escobas_proyecto.png" },
-
-    { id: 2, nombre: "Guantes", precio: 5300,imagen:"./assets/guantes_proyecto.png" },
-
-    { id: 3, nombre: "Microfibras", precio: 2800,imagen:"./assets/microfibras_proyecto.png" },
-
-    { id: 4, nombre: "Regadera", precio: 3700,imagen:"./assets/regaderas_proyecto.png" },
-
-    { id: 5, nombre: "Rejilla", precio: 6000,imagen:"./assets/rejillas_proyecto.png" }
-]
+const URL = "./db/data.json"
 
 let carrito = []
+let productosDisponibles= []
 
 
 const contenedorProductos = document.getElementById('contenedor-productos')
@@ -22,6 +13,17 @@ const totalCarrito = document.getElementById('carrito-total')
 
 const btnFinalizar = document.getElementsByClassName('btn-comprar')[0]
 const btnVaciar = document.getElementsByClassName('btn-secundario')[0]
+
+function obtenerProductos() {
+    fetch(URL)
+    .then(response => response.json())
+    .then(data => {
+        productosDisponibles = data
+        renderizarProductos(productosDisponibles)
+    })
+    .catch(err => console.log("ha ocurrido un error", err))
+    .finally(()=> console.log("ha terminado la peticion"))
+}
 
 function renderizarProductos(lista) {
     contenedorProductos.innerHTML = ''
@@ -42,10 +44,10 @@ function renderizarProductos(lista) {
     const botonesAgregar = document.getElementsByClassName('btn-agregar')
 
     for (const boton of botonesAgregar) {
-        boton.onclick = (evento) => {
+        boton.addEventListener('click',(evento) => {
             const idProductoSeleccionado = evento.target.id
             agregarAlCarrito(idProductoSeleccionado)
-        }
+        })
     }
 }
 
@@ -54,7 +56,7 @@ function agregarAlCarrito(idProducto) {
     const productoElegido = productosDisponibles.find((p) => p.id == idProducto)
     const productoEnCarrito = carrito.find((c)=> c.id == idProducto)
     if(productoEnCarrito)  {
-        agregarcantidad()        
+        agregarcantidad(idProducto)        
     
     } else if (productoElegido) {
        
@@ -108,12 +110,17 @@ function actualizarCarritoDOM() {
         inputsumarcantidad.innerText = "+"
         inputRestarCantidad.innerText = "-"
 
-        inputsumarcantidad.onclick = () => {
-            agregarcantidad(producto.id)
-        }
-        inputRestarCantidad.onclick = () => {
-           restarcantidad(producto.id) 
-        }
+        inputsumarcantidad.addEventListener ('click',() =>{
+           agregarcantidad(producto.id) 
+        })
+            
+        
+        
+        inputRestarCantidad.addEventListener ('click',() =>{
+            restarcantidad(producto.id) 
+        })
+           
+        
         
         const texto = document.createElement('span')
         texto.innerText = ` ${producto.cantidad} - ${producto.nombre} - $${producto.precio} - $${producto.subtotal}`
@@ -125,9 +132,11 @@ function actualizarCarritoDOM() {
         
         btnEliminar.setAttribute('class', 'btn-eliminar')
         
-        btnEliminar.onclick = () => {
-            eliminarDelCarrito(producto.id)
-        }
+        btnEliminar.addEventListener ('click',() => {
+             eliminarDelCarrito(producto.id)
+        })
+            
+        
 
         item.appendChild(inputsumarcantidad)
         item.appendChild(inputRestarCantidad)
@@ -159,21 +168,25 @@ function cargarCarritoLocalStorage() {
 
 
 
-btnVaciar.onclick = () => {
-    carrito = []
+btnVaciar.addEventListener ('click',()=>{
+   carrito = []
     localStorage.removeItem('carritoDetercop')
-    actualizarCarritoDOM()
-}
+    actualizarCarritoDOM() 
+})
+    
 
-btnFinalizar.onclick = () => {
+
+btnFinalizar.addEventListener ('click', () => {
     if (carrito.length > 0) {
         carrito = []
         localStorage.removeItem('carritoDetercop')
         actualizarCarritoDOM()
     }
-}
+})
+   
+
 
 window.onload = () => {
-    renderizarProductos(productosDisponibles)
+    obtenerProductos()
     cargarCarritoLocalStorage()
 }
